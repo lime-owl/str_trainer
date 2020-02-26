b5a887ace0a5952917636298c9bf2da45b307dba
lime-owl <55164193+lime-owl@users.noreply.github.com>
2020-02-26 12:10:34 -0800

    Add files via upload

---
+++ strength_trainer/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace strength_trainer.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
